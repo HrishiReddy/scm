9192d5af78cc05cbcda8cc8fc4d453b1d87d570b
Hrishi Reddy <hrishireddy984@gmail.com>
2021-08-07 01:04:11 +0530

    Update Calculation.java

--- src/main/java/com/example/Calculation.java
+++ src/main/java/com/example/Calculation.java
@@ -6,9 +6,7 @@ public class Calculation {
 		 System.out.println("Hello world");
 		 System.out.println("Calculation");
 		 System.out.println("testing");
-		//http://d1a090e86773.ngrok.io
-
-	//http://d1a090e86773.ngrok.io
+	
 
 	
 
